300ea3c498415d90660e8620d9ae6f88e1056033
Commodity Commodity <commodity@faggot.pw>
2016-04-19 21:19:41 +0100

    First version

---
+++ Scraper/Scraper/DeserializeJSON.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace Scraper
+{
+    // Code from http://stackoverflow.com/a/3806407
+    public class DynamicJson
+    {
+        public static dynamic Deserialize(string inputString)
+        {
+            JavaScriptSerializer mainSerializer = new JavaScriptSerializer();
+            mainSerializer.RegisterConverters(new[] { new DynamicJsonConverter() });
+            return mainSerializer.Deserialize(inputString, typeof(object));
+        }
+    }
+    public sealed class DynamicJsonConverter : JavaScriptConverter
+    {
+        public override object Deserialize(IDictionary<string, object> objectDictionary, Type itemType, JavaScriptSerializer jsSerializer)
+        {
+            if (objectDictionary == null)
+            {
+                throw new ArgumentNullException("objectDictionary");
+            }
+            return itemType == typeof(object) ? new DynamicJsonObject(objectDictionary) : null;
+        }
+        public override IDictionary<string, object> Serialize(object inputObject, JavaScriptSerializer jsSerializer)
+        {
+            throw new NotImplementedException();
+        }
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get { return new ReadOnlyCollection<Type>(new List<Type>(new[] { typeof(object) })); }
+        }
+        private sealed class DynamicJsonObject : DynamicObject
+        {
+            private readonly IDictionary<string, object> objectDictionary;
+
+            public DynamicJsonObject(IDictionary<string, object> objectDictionary)
+            {
+                if (objectDictionary == null)
+                {
+                    throw new ArgumentNullException("objectDictionary");
+                }
+                this.objectDictionary = objectDictionary;
+            }
+            public override string ToString()
+            {
+                var stringBuilder = new StringBuilder("{");
+                ToString(stringBuilder);
+                return stringBuilder.ToString();
+            }
+            private void ToString(StringBuilder stringBuilder)
+            {
+                var firstInDictionary = true;
+                foreach (var keyPair in objectDictionary)
+                {
+                    if (!firstInDictionary)
+                    {
+                        stringBuilder.Append(",");
+                    }
+                    firstInDictionary = false;
+                    var keyValue = keyPair.Value;
+                    var keyName = keyPair.Key;
+                    if (keyValue is string)
+                    {
+                        stringBuilder.AppendFormat("{0}:\"{1}\"", keyName, keyValue);
+                    }
+                    else if (keyValue is IDictionary<string, object>)
+                    {
+                        new DynamicJsonObject((IDictionary<string, object>)keyValue).ToString(stringBuilder);
+                    }
+                    else if (keyValue is ArrayList)
+                    {
+                        stringBuilder.Append(keyName + ":[");
+                        var firstInArray = true;
+                        foreach (var arrayValue in (ArrayList)keyValue)
+                        {
+                            if (!firstInArray)
+                            {
+                                stringBuilder.Append(",");
+                            }
+                            firstInArray = false;
+                            if (arrayValue is IDictionary<string, object>)
+                            {
+                                new DynamicJsonObject((IDictionary<string, object>)arrayValue).ToString(stringBuilder);
+                            }
+                            else if (arrayValue is string)
+                            {
+                                stringBuilder.AppendFormat("\"{0}\"", arrayValue);
+                            }
+                            else
+                            {
+                                stringBuilder.AppendFormat("{0}", arrayValue);
+                            }
+
+                        }
+                        stringBuilder.Append("]");
+                    }
+                    else
+                    {
+                        stringBuilder.AppendFormat("{0}:{1}", keyName, keyValue);
+                    }
+                }
+                stringBuilder.Append("}");
+            }
+            public override bool TryGetMember(GetMemberBinder memberBinder, out object objResult)
+            {
+                if (!objectDictionary.TryGetValue(memberBinder.Name, out objResult))
+                {
+                    objResult = null;
+                    return true;
+                }
+                objResult = WrapResultObject(objResult);
+                return true;
+            }
+            public override bool TryGetIndex(GetIndexBinder memberBinder, object[] indexArray, out object objResult)
+            {
+                if (indexArray.Length == 1 && indexArray[0] != null)
+                {
+                    if (!objectDictionary.TryGetValue(indexArray[0].ToString(), out objResult))
+                    {
+                        objResult = null;
+                        return true;
+                    }
+                    objResult = WrapResultObject(objResult);
+                    return true;
+                }
+                return base.TryGetIndex(memberBinder, indexArray, out objResult);
+            }
+            private static object WrapResultObject(object objResult)
+            {
+                var dictionary = objResult as IDictionary<string, object>;
+                if (dictionary != null)
+                    return new DynamicJsonObject(dictionary);
+
+                var arrayList = objResult as ArrayList;
+                if (arrayList != null && arrayList.Count > 0)
+                {
+                    return (arrayList[0] is IDictionary<string, object>) ? new List<object>(arrayList.Cast<IDictionary<string, object>>().Select(x => new DynamicJsonObject(x))) : new List<object>(arrayList.Cast<object>());
+                }
+                return objResult;
+            }
+        }
+    }
+}
---
+++ Scraper/Scraper/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputURL = "";
+            List<string> albumIDs = new List<string>();
+            List<AlbumImage> allImages = new List<AlbumImage>();
+            bool useGalleryID = false;
+            string dumpDirectory = "";
+            Console.Title = "Input settings";
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Name of dump directory (where everything is saved). Leave blank if you want it to be the gallery ID");
+            Console.ForegroundColor = ConsoleColor.White;
+            dumpDirectory = Console.ReadLine().Trim();
+            if (dumpDirectory.Length == 0)
+            {
+                useGalleryID = true;
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Please enter in the URLs to scrape; one at a time. Enter in 'end' when you are finished. The URLs must be in the format 'https://imgur.com/gallery/XXXXX'");
+            while (inputURL.ToLower() != "end")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                inputURL = Console.ReadLine();
+                if (inputURL.ToLower() == "end")
+                {
+                    continue;
+                }
+                try
+                {
+                    string galleryID = inputURL.Split(new string[] { "gallery/" }, StringSplitOptions.None)[1].Trim(new char[] { '/' });
+                    albumIDs.Add(galleryID);
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine("That URL seems to be in an invalid format!");
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Title = "Scraping URLs";
+            Console.WriteLine("Scraping image URLs...");
+            foreach (string singleAlbum in albumIDs)
+            {
+                try
+                {
+                    string rawDownload = new WebClient().DownloadString("http://imgur.com/ajaxalbums/getimages/" + singleAlbum + "/hit.json?all=true");
+                    dynamic decodedDownload = DynamicJson.Deserialize(rawDownload);
+                    try
+                    {
+                        foreach (dynamic singleItem in decodedDownload.data.images)
+                        {
+                            allImages.Add(new AlbumImage(singleItem, (useGalleryID) ? singleAlbum : dumpDirectory));
+                            try
+                            {
+                                Directory.CreateDirectory((useGalleryID) ? singleAlbum : dumpDirectory);
+                            } catch { }
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("{0}: Invalid URL", singleAlbum);
+                    }
+                } catch { }
+            }
+            Console.WriteLine("Detected a total of {0} image{1}.", allImages.Count().ToString(), (allImages.Count() == 1) ? "" : "s");
+            Console.WriteLine("Beginning download...");
+            string downloadTitleTemplate = "Downloaded {0}/{1} with {2} error(s)";
+            int downloadCount = 0;
+            int errorCount = 0;
+            Parallel.ForEach(allImages.Cast<AlbumImage>(), singleImage =>
+            {
+                if (singleImage.Download())
+                {
+                    downloadCount++;
+                }
+                else
+                {
+                    errorCount++;
+                }
+                Console.Title = string.Format(downloadTitleTemplate, downloadCount.ToString(), allImages.Count().ToString(), errorCount.ToString());
+            });
+            Console.WriteLine("Finished all downloads.");
+            Console.ReadLine();
+        }
+    }
+    public class AlbumImage
+    {
+        string fileName;
+        string fileURL;
+        string filePath;
+        public AlbumImage(dynamic imageData, string galleryID)
+        {
+            fileName = (string)imageData.hash + (string)imageData.ext;
+            fileURL = "http://i.imgur.com/" + fileName;
+            filePath = galleryID + "/" + fileName;
+        }
+        public bool Download()
+        {
+            try
+            {
+                new WebClient().DownloadFile(fileURL, filePath);
+                Console.WriteLine("{0}: Success", fileName);
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("{0}: Failed", fileName);
+                return false;
+            }
+        }
+    }
+}
